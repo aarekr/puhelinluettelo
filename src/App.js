82b2071fcb9ecfb4841ff1ce898101e26b951125
Aare <aare212@hotmail.com>
2020-09-13 16:25:16 +0300

    2.9*: puhelinluettelo step4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: 12345 },
-    { name: 'Grace Hopper', number: 67890 }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchWord, setSearchWord] = useState('')
 
   const addName = (event) => {
       event.preventDefault()
@@ -35,25 +38,30 @@ const App = () => {
   const handleNumberChange = (event) => {
       setNewNumber(event.target.value)
   }
+  const handleSearchWord = (event) => {
+      setSearchWord(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter persons={persons} 
+              searchWord={searchWord.toLowerCase()} 
+              handleSearchWord={handleSearchWord} />
+      <h3>Add a new</h3>
       <form onSubmit={addName}>
           <div>Name: 
-            <input value={newName} 
-              onChange={handleNameChange} />
+            <input value={newName} onChange={handleNameChange} />
           </div>
           <div>Number: 
-            <input value={newNumber}
-              onChange={handleNumberChange} />
+            <input value={newNumber} onChange={handleNumberChange} />
           </div>
           <div>debug: {newName}</div>
           <div>debug: {newNumber}</div>
           <div><button type="submit">add</button></div>
       </form>
 
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
         {persons.map((person, i)=>
             <Person key={i} 
                     name={person.name} 
@@ -68,6 +76,19 @@ const Person = (props) => {
         <div>
             {props.name} {props.number} <br />
         </div>
+    )
+}
+
+const Filter = ({persons, searchWord, handleSearchWord}) => {
+    return(
+      <div>filter shown with <input value={searchWord} onChange={handleSearchWord} />
+        <div>{persons.filter(person => 
+                person.name.toLowerCase().includes(searchWord))
+                .map(filteredPerson => (
+          <p>{filteredPerson.name} {filteredPerson.number}</p>
+          ))}
+        </div>
+      </div>
     )
 }
 
