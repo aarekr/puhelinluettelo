4741011ab8eeea9ec0a1b842358215d8f6b93c25
Aare <aare212@hotmail.com>
2020-11-02 14:10:31 +0200

    2.11: puhelinluettelo step6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter.js'
 import Person from './components/Person.js'
+import axios from 'axios'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  //  { name: 'Arto Hellas', number: '040-123456' },
+  //  { name: 'Ada Lovelace', number: '39-44-5323523' },
+  //  { name: 'Dan Abramov', number: '12-43-234345' },
+  //  { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchWord, setSearchWord] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'persons')
 
   const addName = (event) => {
       event.preventDefault()
